2bc5baa3e9b8fe0885278cc80c0ba1e6542ab194
Ivan <113646650+IvanRiquelme17@users.noreply.github.com>
2022-11-23 01:50:58 -0300

    carrousel terminado
    Falta cambiar las imagenes de la lista

--- carrousel.js
+++ carrousel.js
@@ -1,1 +1,30 @@
- let foto [cookie.jpg ,cupcakes.jpg, mag.jpg ];
+let foto = ['cookie.webp', 'cupcakes.webp', 'mag.webp', 'cake.webp'];
+let contador = 0;
+
+function funcionCarrousel() {
+    let contenedor = document.querySelector(".carrousel");
+    contenedor.addEventListener('click', e => {
+        let derecha = contenedor.querySelector(".derecha");
+        let izquierda = contenedor.querySelector(".izquierda");
+        let imagenes = contenedor.querySelector(".foto1");
+        let target = e.target;
+
+        if (target == izquierda) {
+            if (contador > 0) {
+                imagenes.src = foto[contador - 1];
+                contador--;
+            } else {
+                imagenes.src = foto[foto.length - 1];
+                contador = foto.length;
+            }
+        } else if (target == derecha) {
+            if (contador < foto.length - 1) {
+                imagenes.src = foto[contador + 1];
+                contador++;
+            } else {
+                imagenes.src = foto[0];
+                contador = 0;
+            }
+        }});
+}
+funcionCarrousel();
